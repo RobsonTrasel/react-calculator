7b1f9f7af979b8c875031df4facae6dc0f9b8831
RobsonTrasel <robson.trasel@gmail.com>
2022-11-09 22:25:43 -0300

    feat: button and pad add

---
+++ src/components/Pad/Pad.tsx
@@ -0,0 +1,4 @@
+import React, { FunctionComponent, useEffect } from 'react'
+import styled from 'styled-components'
+import Button from '../Button/Button'
+import { Digit, Operator } from '../../lib/types'
